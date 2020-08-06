aec3ec3b8b02743aab811aadbe7ac7044fe8714b
mac <ms.czerwonka@gmail.com>
2020-08-06 13:24:46 +0200

    Kodilla Task - collections, added OddNumbersExterminator class and CollectionTestSuite

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,2 +1,18 @@
-package com.kodilla.testing.collection;public class OddNumbersExterminator {
+package com.kodilla.testing.collection;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddNumbersExterminator {
+
+    public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (Integer number: numbers) {
+            if (number % 2 == 0) {
+                oddNumbers.add(number);
+            }
+        }
+        return oddNumbers;
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,2 +1,52 @@
-package com.kodilla.testing.collection;public class CollectionTestSuite {
+package com.kodilla.testing.collection;
+
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionTestSuite {
+
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+
+    @DisplayName("when start exterminate method with empty list" +
+            "then method should return empty list"
+    )
+
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        List<Integer> numbers = new ArrayList<>();
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer> result = oddNumbersExterminator.exterminate(numbers);
+        System.out.println("Testing " + result);
+        //Then
+        Assertions.assertEquals(Arrays.asList(), result);
+    }
+
+    @DisplayName("when start exterminate method with not empty list" +
+            "then method should return [4, 8, 10, 12]"
+    )
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        //Given
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, 4, 5, 8, 10, 12));
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer> result = oddNumbersExterminator.exterminate(numbers);
+        System.out.println("Testing " + result);
+        //Then
+        Assertions.assertEquals(Arrays.asList(4, 8, 10, 12), result);
+    }
 }
