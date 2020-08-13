a6dd06423795b4d03c2835ff5878ce188493a162
mac <ms.czerwonka@gmail.com>
2020-08-13 11:39:00 +0200

    added classes to test stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambdaplus.Calculator;
 import com.kodilla.stream.lambdaplus.ExpressionExecutor;
@@ -22,5 +23,8 @@ public class StreamMain {
         poemBeautifier.beautify("this is an example text", (text -> text.concat(" ❤ ").repeat(3)));
         poemBeautifier.beautify("this is an example text", (text -> text.replace("this is an", "XD")));
         poemBeautifier.beautify("this is an example text", (text -> text.replaceAll("an example text", "beautiful code")));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
