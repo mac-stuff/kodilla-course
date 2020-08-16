82b995d121f0eb3100932381574550255cbe34b7
mac <ms.czerwonka@gmail.com>
2020-08-16 11:20:22 +0200

    formated code

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTest.java
@@ -0,0 +1,6 @@
+package com.kodilla.stream.array;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ArrayOperationsTest {
+  
+}
--- kodilla-stream/src/test/java/com/kodilla/stream/forum/ForumUserTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/forum/ForumUserTest.java
@@ -9,7 +9,6 @@ class ForumUserTest {
     @Test
     void testGetLocationsOfFriends() {
         //Given
-        //Create users
         ForumUser user1 = new ForumUser("alan01", "Alan Kosovsky", "Washington");
         ForumUser user2 = new ForumUser("ethanreard", "Ethan Reard", "Miami");
         ForumUser user3 = new ForumUser("eRiK", "Erik Hull", "Key West");
@@ -22,30 +21,20 @@ class ForumUserTest {
         ForumUser user10 = new ForumUser("lawyer", "Lexie John", "Miami");
 
         //When
-        //Create relations for user1
         user1.addFriend(user3);
         user1.addFriend(user7);
         user1.addFriend(user10);
-        //Create relations for user3
         user3.addFriend(user1);
         user3.addFriend(user7);
         user3.addFriend(user2);
         user3.addFriend(user9);
-        //Create relations for user7
         user7.addFriend(user1);
         user7.addFriend(user3);
-        //Create relations for user10
         user10.addFriend(user1);
-        //Create relations for user2
         user2.addFriend(user3);
-        //Create relations for user9
         user9.addFriend(user3);
 
         //Then
-        //user1 has 3 friends with following locations:
-        //      user3:  "Key West"
-        //      user7:  "Washington"
-        //      user10: "Miami"
         assertEquals(3, user1.getLocationsOfFriends().size());
         assertTrue(user1.getLocationsOfFriends().contains("Key West"));
         assertTrue(user1.getLocationsOfFriends().contains("Washington"));
@@ -69,39 +58,24 @@ class ForumUserTest {
         ForumUser user10 = new ForumUser("lawyer", "Lexie John", "Miami");
 
         //When
-        //Create relations for user1
         user1.addFriend(user3);
         user1.addFriend(user7);
         user1.addFriend(user10);
-        //Create relations for user3
         user3.addFriend(user1);
         user3.addFriend(user7);
         user3.addFriend(user2);
         user3.addFriend(user9);
-        //Create relations for user7
         user7.addFriend(user1);
         user7.addFriend(user3);
-        //Create relations for user10
         user10.addFriend(user1);
-        //Create relations for user2
         user2.addFriend(user3);
-        //Create relations for user9
         user9.addFriend(user3);
 
         //Then
-        //user1 has 3 friends with following friends and their locations:
-        //      user3:  user1: - should be skipped ("this" in context of user1)
-        //      user7: "Washington"
-        //      user2: "Miami"
-        //      user9: "Washington" - duplicated, should be skipped
-        //      user7:  user1: - should be skipped ("this" in context of user1)
-        //      user3: "Key West"
-        //      user10: user1: - should be skipped ("this" in context of user1)
         assertEquals(3, user1.getLocationsOfFriendsOfFriends().size());
         assertTrue(user1.getLocationsOfFriendsOfFriends().contains("Key West"));
         assertTrue(user1.getLocationsOfFriendsOfFriends().contains("Washington"));
         assertTrue(user1.getLocationsOfFriendsOfFriends().contains("Miami"));
         assertFalse(user1.getLocationsOfFriendsOfFriends().contains("Orlando"));
     }
-
 }
