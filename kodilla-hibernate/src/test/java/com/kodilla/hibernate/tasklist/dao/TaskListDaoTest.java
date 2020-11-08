e2602b69ed0ddb1dcea46baf46aa02a0b861df8f
marika <ms.czerwonka@gmail.com>
2020-11-09 00:45:01 +0100

    added TaskList class, DAO interface and tests

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTest.java
@@ -0,0 +1,56 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TaskListDaoTest {
+
+    private static final String LISTNAME = "Hibernate";
+    private static final String DESCRIPTION = "Learn Hibernate";
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    void testTaskListDaoSave() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        int id = taskList.getId();
+        Optional<TaskList> readTask = taskListDao.findById(id);
+        assertTrue(readTask.isPresent());
+
+        //CleanUp
+        taskListDao.deleteById(id);
+    }
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+        taskListDao.save(taskList);
+        String name = taskList.getListName();
+
+        //When
+        List<TaskList> readTasks = taskListDao.findByListName(name);
+
+        //Then
+        assertEquals(1, readTasks.size());
+
+        //CleanUp
+        int id = readTasks.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+}
