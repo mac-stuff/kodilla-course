a71475a02217d3ea23122a28fb1e649cda812e1e
marika <ms.czerwonka@gmail.com>
2021-01-28 14:37:24 +0100

    added facade in patterns2

--- kodilla_patterns2/src/test/java/com/kodilla/patterns2/KodillaApplicationTest.java
+++ kodilla_patterns2/src/test/java/com/kodilla/patterns2/KodillaApplicationTest.java
@@ -1,14 +1,13 @@
-package com.kodilla;
+package com.kodilla.patterns2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class KodillaApplicationTest {
 
     @Test
-    public void contextLoad() {
+    void contextLoads() {
     }
 }
