0a87f9fd8f0b30337e9ec8fb22e95128ecc91f83
marika <ms.czerwonka@gmail.com>
2021-01-31 16:47:19 +0100

    added new test

--- kodilla_patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TasksQueueTest.java
+++ kodilla_patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TasksQueueTest.java
@@ -7,6 +7,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class TasksQueueTest {
 
     @Test
+    public void testUpdate() {
+        // Given
+        TasksQueue tasksQueue1 = new TasksQueue("Queue One");
+        TasksQueue tasksQueue2 = new TasksQueue("Queue Two");
+        ObserverService mentorJohnSmith = new ObserverService("John Smith");
+        ObserverService mentorIvonneEscobar= new ObserverService("Ivonne Escobar");
+        ObserverService mentorJessieLinkman = new ObserverService("Jessie Linkman");
+        ObserverService mentorMariaTaylor = new ObserverService("Maria Taylor");
+        tasksQueue1.registerObserver(mentorJohnSmith);
+        tasksQueue1.registerObserver(mentorIvonneEscobar);
+        tasksQueue2.registerObserver(mentorJessieLinkman);
+        tasksQueue2.registerObserver(mentorMariaTaylor);
+
+        // When
+        tasksQueue1.addTask(new Task("Java Task 1", "Test task content"));
+        tasksQueue1.addTask(new Task("Java Task 2", "Test task content"));
+        tasksQueue1.addTask(new Task("Java Task 3", "Test task content"));
+        tasksQueue2.addTask(new Task("Java Task 4", "Test task content"));
+
+        // Then
+        assertEquals(3, mentorJohnSmith.getUpdateCount());
+        assertEquals(1, mentorMariaTaylor.getUpdateCount());
+    }
+
+    @Test
     public void testUpdateGroupOne() {
         // Given
         TasksQueue tasksQueue = new GroupOne();
