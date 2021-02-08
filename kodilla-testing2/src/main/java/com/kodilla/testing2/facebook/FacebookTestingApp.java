0bd70e3cb446d0e155e7c7e9fac39a2971a22b39
marika <ms.czerwonka@gmail.com>
2021-02-08 13:52:09 +0100

    added FacebookTestingApp

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -10,10 +10,9 @@ public class FacebookTestingApp {
 
     public static final String XPATH_ACCEPT_ALL = "//*[@id=\"u_0_h\"]";
     public static final String XPATH_CREATE_ACCOUNT = "//*[@id=\"u_0_2\"]";
-    public static final String XPATH_SIGN_UP = "//*[@id=\"reg_box\"]/div[1]";
 
-    public static final String XPATH_SELECT_DAY = "//*[@id=\"day\"]";
     public static final String XPATH_SELECT_MONTH = "//*[@id=\"month\"]";
+    public static final String XPATH_SELECT_DAY = "//*[@id=\"day\"]";
     public static final String XPATH_SELECT_YEAR = "//*[@id=\"year\"]";
 
     public static void main(String[] args) {
@@ -24,14 +23,15 @@ public class FacebookTestingApp {
         driver.findElement(By.xpath(XPATH_ACCEPT_ALL)).click();
         driver.findElement(By.xpath(XPATH_CREATE_ACCOUNT)).click();
 
-        while (!driver.findElement(By.xpath(XPATH_SIGN_UP)).isDisplayed());
+        while (!driver.findElement(By.xpath(XPATH_SELECT_MONTH)).isDisplayed());
+
+        Select selectMonth = new Select(driver.findElement(By.name(XPATH_SELECT_MONTH)));
+        selectMonth.selectByValue("7");
 
         Select selectDay = new Select(driver.findElement(By.xpath(XPATH_SELECT_DAY)));
-        Select selectMonth = new Select(driver.findElement(By.xpath(XPATH_SELECT_MONTH)));
-        Select selectYear = new Select(driver.findElement(By.xpath(XPATH_SELECT_YEAR)));
-
         selectDay.selectByValue("13");
-        selectMonth.selectByValue("7");
+
+        Select selectYear = new Select(driver.findElement(By.xpath(XPATH_SELECT_YEAR)));
         selectYear.selectByValue("1983");
     }
 }
